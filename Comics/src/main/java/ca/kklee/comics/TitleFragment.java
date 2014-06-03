8c9d786d594f3cbbbce8b1cf67a056d58e9e1e9b
kklee305 <kklee305@gmail.com>
2014-06-03 00:55:39 -0400

    test system notifications

--- Comics/src/main/java/ca/kklee/comics/TitleFragment.java
+++ Comics/src/main/java/ca/kklee/comics/TitleFragment.java
@@ -1,10 +1,15 @@
 package ca.kklee.comics;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
@@ -15,8 +20,26 @@ public class TitleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.title_fragment,container,false);
+        View rootView = inflater.inflate(R.layout.title_fragment, container, false);
         ImageView imageView = (ImageView) rootView.findViewById(R.id.image_view);
+
+        Button test = (Button) rootView.findViewById(R.id.btn_test_notification);
+        test.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("DEBUGGING", "test notifications");
+                        NotificationCompat.Builder mBuilder =
+                                new NotificationCompat.Builder(getActivity().getApplicationContext())
+                                        .setSmallIcon(R.drawable.ic_launcher)
+                                        .setContentTitle("My notification")
+                                        .setContentText("Hello World!");
+                        NotificationManager mNotificationManager =
+                                (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+                        mNotificationManager.notify(1, mBuilder.build());
+                    }
+                }
+        );
 
         return rootView;
     }
