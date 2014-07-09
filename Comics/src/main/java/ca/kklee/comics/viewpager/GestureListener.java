350d885a56d488315fb987bbea928282030739b4
kklee305 <kklee305@gmail.com>
2014-07-09 00:41:10 -0400

    WIP GestureListener for Immersive mode

---
+++ Comics/src/main/java/ca/kklee/comics/viewpager/GestureListener.java
@@ -0,0 +1,53 @@
+package ca.kklee.comics.viewpager;
+
+import android.app.Activity;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+
+import ca.kklee.util.Logger;
+
+/**
+ * Created by Keith on 07/07/2014.
+ */
+public class GestureListener extends SimpleOnGestureListener {
+
+    private View decorView;
+
+    public GestureListener(Activity activity) {
+        decorView = activity.getWindow().getDecorView();
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Logger.d("", "SingleTapUp");
+        if ((decorView.getSystemUiVisibility() & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+            hideUI();
+        } else {
+            showUI();
+        }
+        return true;
+    }
+
+    private void hideUI() {
+        Logger.d("","hide UI");
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+    }
+
+    private void showUI() {
+
+        Logger.d("","show UI");
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+}
