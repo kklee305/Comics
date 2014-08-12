c129d7cfe3561469b6c3397e0dae8d5d88cd7fc8
kklee305 <kklee305@gmail.com>
2014-08-12 02:38:48 -0400

    misc

--- Comics/src/main/java/ca/kklee/util/Config.java
+++ Comics/src/main/java/ca/kklee/util/Config.java
@@ -1,14 +1,13 @@
 package ca.kklee.util;
 
 import android.app.Application;
-import android.content.Context;
 
 /**
  * Created by Keith on 08/06/2014.
  */
 public class Config extends Application {
 
-//    private static Config instance = null;
+    //    private static Config instance = null;
     private static boolean IS_DEBUGGING = true;
     private static boolean IS_LOGGING = true;
 
