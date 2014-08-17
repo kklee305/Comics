cd558adaeeb3960c1f9c29340e82a7d5b284439f
kklee305 <kklee305@gmail.com>
2014-08-17 18:14:35 -0400

    fixed save bitmap
    now does mediascannerconnection scan

--- Comics/src/main/java/ca/kklee/util/Config.java
+++ Comics/src/main/java/ca/kklee/util/Config.java
@@ -1,19 +1,20 @@
 package ca.kklee.util;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by Keith on 08/06/2014.
  */
 public class Config extends Application {
 
-    //    private static Config instance = null;
+    private static Config instance;
     private static boolean IS_DEBUGGING = true;
     private static boolean IS_LOGGING = true;
 
-//    public static Context getContext() {
-//        return instance;
-//    }
+    public static Context getContext() {
+        return instance.getApplicationContext();
+    }
 
     public static boolean IS_DEBUGGING() {
         return IS_DEBUGGING;
@@ -23,9 +24,10 @@ public class Config extends Application {
         return IS_LOGGING;
     }
 
-//    @Override
-//    public void onCreate() {
-//        instance = this;
-//        super.onCreate();
-//    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
 }
