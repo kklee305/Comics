29512af846b2e038b1979ec6230cbbb223baa659
kklee305 <kklee305@gmail.com>
2014-08-18 00:35:26 -0400

    fixed paths

--- Comics/src/main/java/ca/kklee/comics/AppConfig.java
+++ Comics/src/main/java/ca/kklee/comics/AppConfig.java
@@ -1,10 +1,33 @@
 package ca.kklee.comics;
 
-import ca.kklee.util.Config;
+import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by Keith on 27/06/2014.
  */
-public class AppConfig extends Config {
+public class AppConfig extends Application {
     public static final String APPDIRECTORY = "Daily Comics";
+
+    private static AppConfig instance;
+    private static boolean IS_DEBUGGING = true;
+    private static boolean IS_LOGGING = true;
+
+    public static Context getContext() {
+        return instance.getApplicationContext();
+    }
+
+    public static boolean IS_DEBUGGING() {
+        return IS_DEBUGGING;
+    }
+
+    public static boolean IS_LOGGING() {
+        return IS_LOGGING;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
 }
